7c310369e02c7336136e7cbc3d61e7151aeb8102
Rodrigo-200 <84940356+Rodrigo-200@users.noreply.github.com>
2024-04-11 19:02:37 +0100

    s
    s

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, request
+import json
 import git
-import os
 
 app = Flask(__name__)
 
 @app.route('/update', methods=['POST'])
-def update_website():
+def update_data():
     data = request.json
-    # Process the data and update files as necessary
+    print("Received data:", data)
 
-    # Assuming your current directory is a git repository
-    repo_dir = '/path/to/your/repo'  # replace with the path to your repo
+    json_file_path = 'C:\\Users\\rodri\\Downloads\\Projeto-Escola\\data.json'
+
+    with open(json_file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    repo_dir = r'C:\Users\rodri\Downloads\Projeto-Escola'
     repo = git.Repo(repo_dir)
-    repo.git.add(A=True)
-    repo.index.commit("Update from ESP8266")
+    repo.git.add(json_file_path)
+    repo.index.commit("Update sensor data")
     origin = repo.remote(name='origin')
     origin.push()
 
-    return 'Update Successful', 200
+    return 'Data updated', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
