d0361b3d04087c601901a0dccf3ab7ae0bde8d85
Rodrigo-200 <84940356+Rodrigo-200@users.noreply.github.com>
2024-04-12 12:53:33 +0100

    Update sensor data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 import json
 import git
 
@@ -8,15 +9,26 @@ app = Flask(__name__)
 def update_data():
     data = request.json
     print("Received data:", data)
+    json_url = 'https://indigo-guttural-daphne.glitch.me/data.json'
 
-    json_file_path = 'C:\\Users\\rodri\\Downloads\\Projeto-Escola\\data.json'
+    # Fetch the current data
+    response = requests.get(json_url)
+    if response.status_code == 200:
+        current_data = response.json()
+        current_data.update(data)  # Assumes you want to update or add new entries
 
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+        # Update the data back to the server via API (method needs to be implemented on Glitch)
+        update_response = requests.post(json_url, json=current_data)
+        if update_response.status_code == 200:
+            print("Data successfully updated on Glitch")
+        else:
+            print("Failed to update data on Glitch")
+    else:
+        print("Failed to fetch the current data")
 
-    repo_dir = r'C:\Users\rodri\Downloads\Projeto-Escola'
+    repo_dir = r'C:\\Users\\rodri\\Downloads\\Projeto-Escola'
     repo = git.Repo(repo_dir)
-    repo.git.add(json_file_path)
+    repo.git.add('.')
     repo.index.commit("Update sensor data")
     origin = repo.remote(name='origin')
     origin.push()
